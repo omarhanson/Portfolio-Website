fa84dbafc6ab74ae66cfaad479ec40283d4e92ab
omar <omarhanson0@gmail.com>
2022-11-17 16:04:29 -0500

    php

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
 //get data from form  
-$name = $_POST['name'];
-$number = $_POST['number'];
-$email= $_POST['email'];
-$message= $_POST['message'];
+$name = $_POST["name"];
+$number = $_POST["number"];
+$email= $_POST["email"];
+$message= $_POST["message"];
 $to = "omarhanson0@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n  Number = " . $number ."\r\n Message =" . $message;
+$subject = "Mail From Omar Portfolio Website";
+$txt ="name = ". $name . "\r\n  email = " . $email . "\r\n  number = " . $number ."\r\n message =" . $message;
 $headers = "From: noreply@omarportfolio.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
